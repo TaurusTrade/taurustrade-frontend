823b082d74d76301bf2056371c663d024d1fe5d4
Holbrook <wanghaikuo@tsinghua.org.cn>
2021-08-10 13:43:00 +0800

    new import

--- src/main.js
+++ src/main.js
@@ -1,15 +1,66 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import { createApp } from 'vue'
+import { ActionBar, ActionBarIcon, ActionBarButton, Divider, Popup, Overlay, Loading, Dialog, ContactCard, Form, AddressEdit, AddressList, Field, CellGroup, Cell, SwipeCell, Icon, Stepper, Card, Checkbox, CheckboxGroup, Button, Swipe, SwipeItem, PullRefresh, List, Tab, Tabs, SubmitBar, Toast, Skeleton } from 'vant'
+
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+
+import App from './App.vue'
+import store from './store'
 import router from './router'
+import 'lib-flexible/flexible'
 
-Vue.config.productionTip = false
+//import 'vant/lib/index.css'; // 全局引入样式
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+
+
+const app = createApp(App) // 创建实例
+
+// 全局过滤器
+app.config.globalProperties.$filters = {
+  prefix(url) {
+    if (url && url.startsWith('http')) {
+      return url
+    } else {
+      url = `http://backend-api-01.newbee.ltd${url}`
+      return url
+    }
+  }
+}
+
+app.use(ActionBarButton)
+  .use(ActionBarIcon)
+  .use(ActionBar)
+  .use(Divider)
+  .use(Popup)
+  .use(Overlay)
+  .use(Loading)
+  .use(Dialog)
+  .use(Toast)
+  .use(ContactCard)
+  .use(Form)
+  .use(AddressEdit)
+  .use(AddressList)
+  .use(Field)
+  .use(CellGroup)
+  .use(Cell)
+  .use(SwipeCell)
+  .use(Icon)
+  .use(Stepper)
+  .use(Card)
+  .use(Button)
+  .use(Swipe)
+  .use(SwipeItem)
+  .use(PullRefresh)
+  .use(List)
+  .use(Tab)
+  .use(Tabs)
+  .use(SubmitBar)
+  .use(Checkbox)
+  .use(CheckboxGroup)
+  .use(Skeleton)
+
+app.use(router)
+app.use(store)
+
+app.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	Home
+</template>
+
+<script>
+//import { reactive, onMounted, toRefs, nextTick } from 'vue'
+//import { useRouter } from 'vue-router'
+
+export default {
+  name: 'home',
+  components: {
+    //swiper,
+    //navBar
+  },
+  setup() {
+    //const router = useRouter()
+  }
+}
+
+</script>
+
